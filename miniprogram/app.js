40478cd55c666ae4974bc9b738ac2ff0f7248dba
GK0421 <gk_24@users.noreply.github.com>
2026-05-18 22:41:13 +0800

    fix: add wx.cloud.init with cloud env ID in app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,17 @@
 // 小程序全局逻辑
 App({
   globalData: {
-    cloudEnv: '' // 云环境ID
+    cloudEnv: 'cloud1-d1gwyutj2d122bc22'
+  },
+  onLaunch: function () {
+    // 初始化云开发
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+      wx.cloud.init({
+        env: 'cloud1-d1gwyutj2d122bc22',
+        traceUser: true,
+      });
+    }
   }
 });
